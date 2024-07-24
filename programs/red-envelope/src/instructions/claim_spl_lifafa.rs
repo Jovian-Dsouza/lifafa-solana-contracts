b9aa8b60b11c89836722b2b1f7606d17d22b633c
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-07-24 05:20:16 +0000

    feat: refactor claim lifafa

--- programs/red-envelope/src/instructions/claim_spl_lifafa.rs
+++ programs/red-envelope/src/instructions/claim_spl_lifafa.rs
@@ -13,56 +13,47 @@ use anchor_spl::{
 
 
 pub fn claim_spl_lifafa(ctx: Context<ClaimSplLifafa>, _id: u64) -> Result<()>  {
-        // let lifafa = &mut ctx.accounts.lifafa;
-        let user_claim = &mut ctx.accounts.user_claim;
+    let user_claim = &mut ctx.accounts.user_claim;
 
-        let clock = Clock::get()?;
-        let time_left = (ctx.accounts.lifafa.creation_time + ctx.accounts.lifafa.time_limit) - clock.unix_timestamp;
-        if time_left <= 0 {
-            return err!(LifafaError::TimeLimitExpired); // Time limit exceeded
-        }
-
-        // Check maximum claims limit
-        if ctx.accounts.lifafa.claims >= ctx.accounts.lifafa.max_claims {
-            return err!(LifafaError::MaxClaimsReached); 
-        }
-
-        if user_claim.claimed == true {
-            return err!(LifafaError::AlreadyClaimed);
-        }
-
-        // TODO use Oracle for Security - however cost of 0.002 SOL will be added
-        // Pseudo random number - Uses timestamp, hash and xorshift
-        let hash = hash(&clock.slot.to_be_bytes());
-        let pseudo_random_number = xor_shift(u64::from_be_bytes(hash.to_bytes()[..8].try_into().unwrap()));
-
-        
-        // Fetch the maximum withdrawable balance from the vault token account
-        let max_withdrawable_balance = ctx.accounts.vault.amount;
-
-        let claim_amount =  pseudo_random_number % max_withdrawable_balance; // TODO random amount, between 0 and max withdrawable balance
-        let transfer_accounts = TransferChecked {
-            from: ctx.accounts.vault.to_account_info(),
-            mint: ctx.accounts.mint.to_account_info(),
-            to: ctx.accounts.ata.to_account_info(),
-            authority: ctx.accounts.lifafa.to_account_info(),
-        };
-
-        let seeds: [&[&[u8]]; 1] = [&[LIFAFA_SEED.as_bytes(), &_id.to_le_bytes(), &[ctx.accounts.lifafa.bump]]];
-        let cpi_ctx = CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), transfer_accounts, &seeds);
-
-        let _ = transfer_checked(cpi_ctx, claim_amount, ctx.accounts.mint.decimals)?;
-
-        // let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), transfer_accounts);
-        // let _ = transfer_checked(cpi_ctx, claim_amount, ctx.accounts.mint.decimals);
-
-        ctx.accounts.lifafa.claims += 1;
-        user_claim.claimed = true;
-        user_claim.amount_claimed = claim_amount;
-
-        msg!("Lifafa Claimed, Amount: {}", claim_amount);
-        Ok(())
+    let clock = Clock::get()?;
+    let time_left = (ctx.accounts.lifafa.creation_time + ctx.accounts.lifafa.time_limit) - clock.unix_timestamp;
+    if time_left <= 0 {
+        return err!(LifafaError::TimeLimitExpired); // Time limit exceeded
     }
+
+    // Check maximum claims limit
+    if ctx.accounts.lifafa.claims >= ctx.accounts.lifafa.max_claims {
+        return err!(LifafaError::MaxClaimsReached); 
+    }
+
+    if user_claim.claimed == true {
+        return err!(LifafaError::AlreadyClaimed);
+    }
+
+    // TODO use Oracle for Security - however cost of 0.002 SOL will be added
+    // Pseudo random number - Uses timestamp, hash and xorshift
+    let hash = hash(&clock.slot.to_be_bytes());
+    let pseudo_random_number = xor_shift(u64::from_be_bytes(hash.to_bytes()[..8].try_into().unwrap()));
+    let claim_amount =  pseudo_random_number % ctx.accounts.vault.amount;
+    
+    let transfer_accounts = TransferChecked {
+        from: ctx.accounts.vault.to_account_info(),
+        mint: ctx.accounts.mint.to_account_info(),
+        to: ctx.accounts.ata.to_account_info(),
+        authority: ctx.accounts.lifafa.to_account_info(),
+    };
+
+    let seeds: [&[&[u8]]; 1] = [&[LIFAFA_SEED.as_bytes(), &_id.to_le_bytes(), &[ctx.accounts.lifafa.bump]]];
+    let cpi_ctx = CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), transfer_accounts, &seeds);
+    let _ = transfer_checked(cpi_ctx, claim_amount, ctx.accounts.mint.decimals)?;
+
+    ctx.accounts.lifafa.claims += 1;
+    user_claim.claimed = true;
+    user_claim.amount_claimed = claim_amount;
+
+    msg!("Lifafa Claimed, Amount: {}", claim_amount);
+    Ok(())
+}
 
 #[derive(Accounts)]
 #[instruction(_id : u64)]
